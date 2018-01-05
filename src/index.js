4e29ac857859ce509962dfab5665ae4822c845e0
Reinaldo Schiehll <rn.schiehll@gmail.com>
2018-01-05 21:54:24 -0200

    Fix close button styles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 const alertStyle = {
   backgroundColor: '#333333',
   color: 'white',
-  padding: '10px',
   borderRadius: '2px',
   display: 'flex',
   justifyContent: 'space-between',
@@ -11,7 +10,16 @@ const alertStyle = {
   boxShadow: '0 8px 12px 0 rgba(0,0,0,0.3)',
   width: '300px',
   boxSizing: 'border-box',
-  fontSize: '11px'
+  fontSize: '11px',
+  position: 'relative'
+}
+
+const contentWrapperStyle = {
+  padding: '10px 60px 10px 10px',
+  display: 'flex',
+  width: '100%',
+  justifyContent: 'center',
+  alignItems: 'center'
 }
 
 const iconPlaceholderStyle = {
@@ -24,17 +32,15 @@ const messageStyle = {
   flex: 3,
   textAlign: 'center',
   textTransform: 'uppercase',
-  padding: '8px 10px',
-  marginRight: '50px'
+  width: '100%'
 }
 
 const closeButtonStyle = {
-  width: '50px',
+  minWidth: '50px',
   height: '100%',
-  lineHeight: '50px',
+  position: 'absolute',
   borderRadius: '0 2px 2px 0',
   cursor: 'pointer',
-  position: 'absolute',
   top: 0,
   right: 0,
   backgroundColor: '#444',
@@ -56,13 +62,15 @@ const getIcon = type => {
 
 const AlertTemplate = ({ message, options, style, close }) => (
   <div style={{ ...alertStyle, ...style }}>
-    <div style={iconPlaceholderStyle}>
-      <div style={{ width: 32, height: 32, background: getIcon(options.type) }} />
+    <div style={contentWrapperStyle}>
+      <div style={iconPlaceholderStyle}>
+        <div style={{ width: 32, height: 32, background: getIcon(options.type) }} />
+      </div>
+      <div style={messageStyle}>{message}</div>
     </div>
-    <div style={messageStyle}>{message}</div>
     <div
       onClick={close}
-      style={{ ...closeButtonStyle, right: options.offset }}
+      style={closeButtonStyle}
     >
       <svg fill='white' xmlns='http://www.w3.org/2000/svg' height='25' width='25' viewBox='0 0 48 48'>
         <path d='M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z' />
